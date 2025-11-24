f08494a73a83b38351f5d3aa98b39f454c25496e
Paul Clark <pc235603@gmail.com>
2025-11-24 10:12:31 +0000

    Preserve the data pointer

--- src/SparkFun_Extensible_Message_Parser.cpp
+++ src/SparkFun_Extensible_Message_Parser.cpp
@@ -397,8 +397,12 @@ void sempParseNextByte(SEMP_PARSE_STATE *parse, uint8_t data)
 // Parse the next bytes
 void sempParseNextBytes(SEMP_PARSE_STATE *parse, uint8_t *data, uint16_t len)
 {
+    uint8_t *ptr = data;
     for (uint16_t i = 0; i < len; i++)
-        sempParseNextByte(parse, *(data++));
+    {
+        sempParseNextByte(parse, *ptr);
+        ptr++;
+    }
 }
 
 // Shutdown the parser

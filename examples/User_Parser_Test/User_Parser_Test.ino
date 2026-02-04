c0a0c5eb0dfba69f0779b4e9648b56683a1a2db0
Lee Leahy <lpleahyjr@gmail.com>
2026-02-04 07:18:26 -1000

    Arduino Uno R3: Add support to build for Arduino Uno R3

--- examples/User_Parser_Test/User_Parser_Test.ino
+++ examples/User_Parser_Test/User_Parser_Test.ino
@@ -8,6 +8,7 @@
 */
 
 #include <SparkFun_Extensible_Message_Parser.h> //http://librarymanager/All#SparkFun_Extensible_Message_Parser
+#include "Arduino_Boards.h"
 #include "ESP32.h"
 #include "SAMD21.h"
 #include "SAMD51.h"
